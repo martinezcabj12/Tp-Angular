9179c529a762cda24ca03dceaa1e1b10f959deed
MartinezDario <darosmd@DESKTOP-9B1DEFD>
2024-05-03 03:44:37 -0300

    Estructura | rutas

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  standalone: true,
+  imports: [],
+})
+export class HeavyLoadersSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+})
+
+export class UserLoaderComponent {
+  constructor() {
+  }
+}
+
